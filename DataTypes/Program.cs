a7ed956f64a367999e7221c252cbe12e4d612822
Artem <Artem@DESKTOP-TMFRGH4>
2026-05-20 12:52:30 +0300

    DataTypes in progress

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -8,8 +8,52 @@ namespace DataTypes
 {
     internal class Program
     {
+        static string delimiter = "----------------------------------------------";
         static void Main(string[] args)
         {
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine(sizeof(bool));
+            Console.WriteLine(true.GetType());
+
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("char");
+            Console.ResetColor();
+            Console.WriteLine(sizeof(char));
+            Console.WriteLine(delimiter);
+
+            //byte sbyte - Signed byte
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("byte");
+            Console.ResetColor();
+            Console.WriteLine(sizeof(byte) + " byte");
+            Console.WriteLine($"Range:{byte.MinValue} ... {byte.MaxValue}");
+            Console.WriteLine(((byte)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("sbyte");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(sbyte)} byte");
+            Console.WriteLine($"Range:{byte.MinValue} ... {byte.MaxValue}");
+            Console.WriteLine(((byte)0).GetType());
+            Console.WriteLine(delimiter);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("short");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(short)}");
+            Console.WriteLine($"Range:{short.MinValue} ... {short.MaxValue}");
+            Console.WriteLine(((short)0).GetType());
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("ushort");
+            Console.ResetColor();
+            Console.WriteLine($"Size: {sizeof(short)}");
+            Console.WriteLine($"Range:{ushort.MinValue} ... {short.MaxValue}");
+            Console.WriteLine(((ushort)0).GetType());
+
+
         }
     }
 }
