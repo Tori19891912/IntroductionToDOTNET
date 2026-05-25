69d3e28664f183007a497944f693f75dcddc2979
Artem <Artem@DESKTOP-TMFRGH4>
2026-05-25 12:52:34 +0300

    Game in progress

---
+++ Game/Program.cs
@@ -0,0 +1,84 @@
+﻿//#define CHAR_CONTROL
+//#define SHOOTER
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if CHAR_CONTROL
+            char key;
+            do
+            {
+                key = Console.ReadKey(true).KeyChar;
+                //Console.WriteLine($"{(int)key}\t{key}");
+                switch (key)
+                {
+                    case 'w': Console.WriteLine("Вверх"); break;
+                    case 's': Console.WriteLine("Вниз"); break;
+                    case 'a': Console.WriteLine("Влево"); break;
+                    case 'd': Console.WriteLine("Вправо"); break;
+                }
+            }while(true);
+#endif
+
+#if SHOOTER
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+                //Console.WriteLine($"{key}");
+                switch (key)
+                {
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("Вверх"); break;
+                    case ConsoleKey.S:
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("Вниз"); break;
+                    case ConsoleKey.A: Console.WriteLine("Влево"); break;
+                    case ConsoleKey.D: Console.WriteLine("Вправо"); break;
+                    case ConsoleKey.Spacebar: Console.WriteLine("Прыжок"); break;
+                    case ConsoleKey.Enter: Console.WriteLine("Огонь"); break;
+                }
+            } while (key != ConsoleKey.Escape);
+#endif 
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            ConsoleKey key;
+            int x=10;
+            int y=10;
+            const char symbol = '@';
+            do
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine(symbol);
+                key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                    case ConsoleKey.W:      y--;  break;
+
+                    case ConsoleKey.S:      y--; break;
+                    case ConsoleKey.LeftArrow:
+                        
+                    case ConsoleKey.A:  x--; break;
+                    case ConsoleKey.RightArrow:
+                    case ConsoleKey.D: Console.WriteLine("Вправо"); break;
+                   
+                }
+                if(x<0) x= 0;
+                if(y < 0) y = 0;
+                if (x == 0 || y == 0) Console.Beep();
+                //TODO: Задать размер консоли
+            } while (key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ дз 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("дз 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("дз 1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c3eb134-77f5-4859-914e-ea6f9486e9ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
